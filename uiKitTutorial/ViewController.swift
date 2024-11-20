d882eb05cb3f78c6ba14c73c2a265eb9fd2d0c1a
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-20 11:49:04 -0300

    Button: see Actions with or without storyBoard

--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -14,9 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         
+        // create button without storyboard
+        let button = UIButton(type: .system)
+        button.setTitle("Push me", for: .normal)
+        button.setTitle("Pushed", for: .highlighted)
+        button.frame = CGRect(x:20,y: 20,width: 200,height: 100)
+        button.addTarget(self, action: #selector(doAnotherThing), for: .touchUpInside)
+        view.addSubview(button)
         
     }
 
-
+    @objc func doAnotherThing(_button:UIButton){
+        print("Pushed")
+    }
+    
+    @IBAction func doSomething(_ sender: Any) {
+        print("Hello")
+    }
+    
+    @IBAction func touchDown(_ sender: Any){ // Any can be chaged for UIComponents
+        print("Button Down")
+    }
+    
+    @IBAction func sliderChanged(_ slider: UISlider) {
+        print("Value Changed \(slider.value)")
+    }
+    
 }
 
