7eb72f46f39018267aaf27fa53ae40b327b8918f
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-20 10:20:13 -0300

    View: add some view

--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue
+        let myView = UIView(frame: CGRect(x:50,y:50,width: 100,height: 100))
+        myView.backgroundColor = .magenta
+        
+        view.addSubview(myView)
     }
 
 
