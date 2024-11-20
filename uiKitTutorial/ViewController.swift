6cbbbc422044c561f1c6ff63a8c38a23397a6f99
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-20 10:08:50 -0300

    Initial Commit

---
+++ uiKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uiKitTutorial
+//
+//  Created by Davi Paiva on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ uiKitTutorialTests/uiKitTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  uiKitTutorialTests.swift
+//  uiKitTutorialTests
+//
+//  Created by Davi Paiva on 20/11/24.
+//
+
+import Testing
+@testable import uiKitTutorial
+
+struct uiKitTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
