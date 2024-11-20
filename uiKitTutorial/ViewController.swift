180d59225712bade5d5ad7c3e852a23a0203f9fd
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-20 10:36:33 -0300

    View: more attr

--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -13,10 +13,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.blue
+        
+        // MARK: - Views Tutorial
         let myView = UIView(frame: CGRect(x:50,y:50,width: 100,height: 100))
         myView.backgroundColor = .magenta
+        myView.clipsToBounds = true // clip the sub view
+        myView.alpha = 0.5 // opacity attr
         
+        // more info about graphics  :https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_overview/dq_overview.html
+        
+        myView.transform = CGAffineTransform(rotationAngle: CGFloat.pi/4) // rotate view
+        
+        // show attr of the view
+        print(myView.frame) // frame position
+        print(myView.bounds) // frame bounds
+        print(myView.center) // center of the frame
+        
+        // add some subview
+        let subView = UIView(frame: CGRect(x:50,y: 50,width: 100,height: 100))
+        subView.backgroundColor = .red
+        myView.isHidden = false // hide or not the view
+        
+        // relate view
+        myView.addSubview(subView)
         view.addSubview(myView)
+        
+        // MARK: - Labels Tutorial
+        
+        
     }
 
 
