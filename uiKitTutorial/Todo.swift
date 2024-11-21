ceaa62a7580883b0b1469f38823e80fa2018ea79
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-20 21:56:58 -0300

    UITableView: add sections, rows and cells

---
+++ uiKitTutorial/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  uiKitTutorial
+//
+//  Created by Davi Paiva on 20/11/24.
+//
+
+import Foundation
+
+struct Todo{
+    let title: String
+    let isComplete: Bool = false
+}
--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // empty project
+    
+    let todos = [
+        Todo(title: "Make a vanilla pudding"),
+        Todo(title: "Put pudding in a mayo jarl"),
+        Todo(title: "Eat in a public place"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .white
-        
         
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+ 
+    /* define number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+     */
+    
+    // define number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    // define cells 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // print(indexPath)
+        
+        let todo = todos[indexPath.row]
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = todo.title
+        return cell
+    }
+    
+    
+    
+    
+}
