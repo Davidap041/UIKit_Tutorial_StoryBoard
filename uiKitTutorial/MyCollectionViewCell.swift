98a15d652dd47ca844f794d0553ea07aeca41721
Davi Paiva <davialexandre_dap@hotmail.com>
2024-11-21 22:13:38 -0300

    UICollectionView: use UICollectionViewCompositionalLayout

---
+++ uiKitTutorial/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  uiKitTutorial
+//
+//  Created by Davi Paiva on 21/11/24.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -8,15 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // empty project
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .white
-        
-        
+        collectionView.collectionViewLayout = layout()
     }
-
-
+    func layout() -> UICollectionViewCompositionalLayout{
+        // item layout
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.2),
+            heightDimension: .fractionalHeight(1.0))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // group layout
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(50))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .flexible(20)
+        
+        // section layout
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 50
+        
+        // layout
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        // config
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 200
+        layout.configuration = config
+        
+        return layout
+    }
 }
 
+extension ViewController: UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReusable", for: indexPath) as! MyCollectionViewCell
+        
+        cell.backgroundColor = UIColor.systemCyan
+        cell.label.text = String(indexPath.item)
+        return cell
+    }
+    
+    
+}
