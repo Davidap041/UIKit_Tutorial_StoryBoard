073eb407a568511cdbf982e505babe7482917295
Davi Paiva <davialexandre_dap@hotmail.com>
2024-12-11 21:14:27 -0300

    functional call

--- uiKitTutorial/ViewController.swift
+++ uiKitTutorial/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     // empty project
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,7 +18,26 @@ class ViewController: UIViewController {
         
         
     }
-
-
+    
+    @IBAction func randomQuote(_ sender: Any) {
+        let url = URL(string: "https://api.kanye.rest/")!
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            // task completed with error or not
+            (data: Data?, response: URLResponse?,error: Error?) in
+            if let error = error {
+                print("Error: ", error)
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data!,options: []) as! [String: String]
+            
+            DispatchQueue.main.async{
+                self.label.text = json["quote"]
+            }
+        }
+        task.resume()
+    }
+    
 }
 
